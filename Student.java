e2a963b737b3a19eaf4d58bc8c6646b61d4ac432
soficastro <38478765+soficastro@users.noreply.github.com>
2018-12-16 17:21:12 -0300

    Update Student.java

--- Student.java
+++ Student.java
@@ -6,25 +6,28 @@ public class Student
     String name;
     String email;
     ArrayList<Project> student_projects;
-    //ArrayList<Publication> student_publications;
+    ArrayList<Publication> student_publications;
     int type; //Type 1 = Undergrad, type 2 = Grad; type 3 = Master
 
     public Student(String name, String email, int type)
     {
         this.name = name;
         this.email = email;
-        student_projects = new ArrayList<Project>();
-        //student_publications =  = new ArrayList<Publication>();
+        this.student_projects = new ArrayList<Project>();
+        this.student_publications = new ArrayList<Publication>();
         this.type = type;
     }
 
     public String getEmail() {
         return this.email;
     }
+    public String getName() {
+        return this.name;
+    }
 
     public void display()
     {
-        System.out.println(this.name);
+        System.out.println("\n" + this.name);
         if(this.type == 1)
         {
             System.out.println("Undergraduate student");
@@ -38,9 +41,14 @@ public class Student
             System.out.println("Master's student");
         }
         System.out.println(this.email);
+        for (Project i : this.student_projects) {
+            System.out.println(i.getTitle());
+        }
+        /*
+        for (Publication i : student_publications) {
+            System.out.println(i.getTitle());
+        }*/
+
+        System.out.println("\n");
     }
-
-    //public boolean findStudent
-
-
 }
