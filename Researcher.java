d801ada801a0c4deadfcadc5b874dd89b5350d5c
soficastro <38478765+soficastro@users.noreply.github.com>
2018-12-16 17:20:23 -0300

    Update Researcher.java

--- Researcher.java
+++ Researcher.java
@@ -1,19 +1,18 @@
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Researcher
 {
     String name;
     String email;
     ArrayList<Project> researcher_projects;
-    //ArrayList<Publication> student_publications;
+    ArrayList<Publication> researcher_publications;
 
     public Researcher(String name, String email)
     {
         this.name = name;
         this.email = email;
-        researcher_projects = new ArrayList<Project>();
-        //student_publications =  = new ArrayList<Publication>();
+        this.researcher_projects = new ArrayList<Project>();
+        this.researcher_publications = new ArrayList<Publication>();
     }
 
     public String getEmail() {
@@ -24,9 +23,12 @@ public class Researcher
     {
         System.out.println(this.name + "\nResearcher");
         System.out.println(this.email);
+        for (Project i : this.researcher_projects) {
+            System.out.println(i.getTitle());
+        }
+        /*
+        for (Publication i : researcher_publications) {
+            System.out.println(i.getTitle());
+        }*/
     }
-
-
-
-
 }
